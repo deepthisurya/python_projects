c2eb409faafde301ec6c2a61c4fb96e9e4b7242a
Deepthi Challa <deepthisurya.v@gmail.com>
2023-12-28 14:29:13 +0530

    *Create a random number list *get the maximum number from the list *get Minimum number from the list

---
+++ max_min.py
@@ -0,0 +1,28 @@
+# From the list of numbers , find the maximum and minumum number
+import random
+def get_num_list():
+    list = []
+    for x in range(20):
+        list.append(random.randint(3,9))
+    return list
+
+def getMin(list):
+    min_num = list[0]
+    for x in list:
+        if(x < min_num):
+            min_num = x
+    return min_num
+
+def getMax(list):
+    max_num = list[0]
+    for x in list:
+        if (x > max_num):
+            max_num = x
+    return max_num
+
+
+if __name__ == '__main__':
+    num_list = get_num_list()
+    print(num_list)
+    print(getMax(num_list))
+    print(getMin(num_list))
